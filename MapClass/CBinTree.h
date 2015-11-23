410284580fb2938c42cd0b40165170081b4405fe
SalahEddin <salshaal@gmail.com>
2015-11-23 11:22:41 +0200

    removed generic code, will use int for key and value

---
+++ MapClass/CBinTree.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef CBINTREE
+#define CBINTREE
+
+class CBinTree
+{
+public:
+	CBinTree();
+	~CBinTree();
+
+private:
+
+};
+
+CBinTree::CBinTree()
+{
+}
+
+CBinTree::~CBinTree()
+{
+}
+#endif
